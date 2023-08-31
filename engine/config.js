59a598173bd1f521f1722b7d086f29b724331982
phithagod24 <94188317+phithagod24@users.noreply.github.com>
2023-08-31 05:54:55 +0100

    Add files via upload

---
+++ engine/config.js
@@ -0,0 +1,18 @@
+
+// Motobug engine configuration flags
+var configuration = {
+    classicAngles: false,   // snap sonic to 45deg, just like the classics
+	mBlurDefault: true,      // set to false to disable motion blur
+	skipMenus: false,		// set to true to skip the menus and go straight
+							// into the gameplay
+	startLevel: 0,			// the index of the first level to play
+	
+							//Default keybinds
+	lKey: 37,				//Left keymap
+	rKey: 39,				//Right keymap
+	upKey: 38,				//Up keymap
+	dKey: 40,				//Down keymap
+	jKey: 65,				//Jump key
+	startKey: 13			//Start Button
+};
+
